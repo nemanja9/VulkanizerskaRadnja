1dea1270ed8fb0cce3441fa0245f10858646410e
nemanjajurisic <nemanjajurisic@192.168.0.18>
2018-03-20 00:34:34 +0100

    Dodat JavaDoc za klasu VulkanizerskaRadnja

--- VulkanizerskaRadnja/VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -3,9 +3,23 @@ package gume.radnja;
 import java.util.LinkedList;
 import gume.AutoGuma;
 
+/**
+ * Klasa koja predstavlja vulkanizersku radnju
+ * 
+ * @author Nemanja Jurisic
+ * @version 1.0
+ *
+ */
 public class VulkanizerskaRadnja {
+	/** Atribut koji predstavlja skup guma */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+	/**
+	 * Metoda koja dodaje novu gumu u listu guma
+	 * @param a guma koja treba da bude dodata u listu guma
+	 * @throws java.lang.NullPointerException ako je prosleđen null kao parametar
+	 * @throws java.lang.RuntimeException ako guma koju pokusavamo da dodamo vec postoji u listi
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -13,7 +27,13 @@ public class VulkanizerskaRadnja {
 			throw new RuntimeException("Guma vec postoji");
 		gume.addFirst(a);
 	}
-
+	/**
+	 * Metoda koja na osnovu unetog Stringa markaModel pronalazi
+	 * istu takvu gumu
+	 * @param markaModel marka i model gume koju trazimo u obliku Stringa
+	 * @return novaLista lista guma koje imaju dati model i marku
+	 * @return null ako je prosledjen null
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
